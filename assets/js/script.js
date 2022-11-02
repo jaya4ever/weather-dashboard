138034aa9923dfe2e3a20dcd5894253a234b498b
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-11-02 15:05:55 -0500

    added id for history and defined variables in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
 //look for search city
 //look for click search button
 //look for history stored after user searched 
+
+var searchCity = document.getElementById("#search-city");
+var startSearch = document.getElementById("#search-button");
+var serachHistory = [];
