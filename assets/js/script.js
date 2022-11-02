499de05aabd73606dc0f2d846a68a34718538f42
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-11-02 18:28:04 -0500

    working on the local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@
 //look for click search button
 //look for history stored after user searched 
 
+var apiKey = "8d6fedb89a89e930cd42aacc3d71bd01";
+var currentDay = moment().format();
+
 var searchCity = document.getElementById("#search-city");
 var startSearch = document.getElementById("#search-button");
 var historyStored = document.getElementById("#history");
@@ -9,7 +12,22 @@ var historyStored = document.getElementById("#history");
 var searchForTheCity ="";
 var historyOfTheCity =[];
 
-startSearch.addEventListener("click", function (e){
-    e.preventDefault();
+// pulls the city array from the local storage
+function cityList(){
+    var storedCities = Json.parse(localStorage.getItem("cities"));
+    if(storedCities !== null){
+        historyOfTheCity = storedCities;
+    }
+}
+
+function weatherList(){
+    var storedWeather = Json.parse(localStorage.getItem("currentCity"));
+    if(storedWeather !==null){
+        searchForTheCity = storedWeather;
+    }
+}
+
+startSearch.addEventListener("click", function (event){
+    event.preventDefault();
  
 })
