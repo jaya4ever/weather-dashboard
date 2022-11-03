25c00a65d3385d23d70c5d7571e95a8a99ca522c
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-11-03 15:42:41 -0500

    added fetch and tried it in the browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,13 @@
 //look for click search button
 //look for history stored after user search
 
-//var apiKey = "8d6fedb89a89e930cd42aacc3d71bd01";
+var apiKey = "8d6fedb89a89e930cd42aacc3d71bd01";
 var currentDay = moment().format("h:mm:ss a");
 
-var searchCity = document.getElementById("#search-city");
-var startSearch = document.getElementById("#search");
-var historyStored = document.getElementById("#history");
+var searchCity = document.getElementById("city");
+var startSearch = document.getElementById("search");
+var historyStored = document.getElementById("history");
+
 
 var searchForTheCity ="";
 var historyOfTheCity =[];
@@ -29,12 +30,15 @@ function weatherList(){
     }
 }
 
-function fetchData(){
+/*function fetchData(){
     var apiKey = "8d6fedb89a89e930cd42aacc3d71bd01";
-    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&appid=${apiKey}`)
+    //fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${searchCity.value}&appid=${apiKey}`)
     .then(response => response.json())
     .then(geoData => {
-            return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${geoData[0].lat}&lon=${geoData[0].lon}&appid=${apiKey}&exclude=hourly,minutely&units=imperial`)
+        //
+https://api.openweathermap.org/data/2.5/forecast?lat=42.3554334&lon=-71.060511&appid=8d6fedb89a89e930cd42aacc3d71bd01
+//https://api.openweathermap.org/geo/1.0/direct?q=boston&appid=8d6fedb89a89e930cd42aacc3d71bd01
+    //return fetch(`https://api.openweathermap.org/2.5/forecast?lat=${geoData[0].lat}&lon=${geoData[0].lon}&appid=${apiKey}&exclude=hourly,minutely&units=imperial`)
     })
     .then(response => response.json())
     .then(cityData => {
@@ -46,9 +50,28 @@ function fetchData(){
   console.log(cityData);
     })
 
-}
+}*/
 
 startSearch.addEventListener("click", function (event){
     event.preventDefault();
+    //fetchData();
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${searchCity.value}&appid=8d6fedb89a89e930cd42aacc3d71bd01`)
+    .then(response => response.json())
+    .then(geoData => {
+        //console.log(geoData);
+       fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${geoData[0].lat}&lon=${geoData[0].lon}&appid=8d6fedb89a89e930cd42aacc3d71bd01`)
+        .then(response => response.json())
+        .then(weatherData => {
+            console.log(weatherData);
+            var weatherList = weatherData.list;
+            //console.log(weatherList);
+            for(var i=0; i < 5; i++ ){
+                
+            }
+
+        })
+        //console.log(geoData[0].lat);
+        //console.log(geoData[0].lon);
+    })
  
 })
