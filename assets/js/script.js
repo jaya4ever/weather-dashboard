765003eca23be05bb989e56442638141dd8cb419
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-11-03 11:23:14 -0500

    modyfied js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //look for search city
 //look for click search button
-//look for history stored after user searched 
+//look for history stored after user search
 
 var apiKey = "8d6fedb89a89e930cd42aacc3d71bd01";
 var currentDay = moment().format();
