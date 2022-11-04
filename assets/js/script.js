d68ee095bbd2bcb65e795b81b20ca0a7f6c6d178
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-11-04 17:32:04 -0500

    creating elements in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,102 +8,123 @@ var currentDay = moment().format("h:mm:ss a");
 var searchCity = document.getElementById("city");
 var startSearch = document.getElementById("search");
 var historyStored = document.getElementById("history");
+var sectionContainer = document.getElementById("today-weather")
 
 
 var searchForTheCity = "";
-var historyOfTheCity = [];
+// var historyOfTheCity = [];
 
 // pulls the city array from the local storage
-function cityList() {
-    var searchedCities = Json.parse(localStorage.getItem("City"));
-    if (searchedCities !== undefined) {
-        historyOfTheCity = searchedCities;
+// function cityList() {
+//     var searchedCities = Json.parse(localStorage.getItem("City"));
+//     if (searchedCities !== undefined) {
+//         historyOfTheCity = searchedCities;
 
-    }
+//     }
 
-}
+// }
 
 
 /*function weatherList() {
     var storedWeather = Json.parse(localStorage.getItem("currentCity"));
     if (storedWeather !== null) {
         searchForTheCity = storedWeather;
-
+        
     }
 }*/
+function startFunction(){
+   var userInput = searchCity.value;
+    console.log(userInput);
 
-
-
-
-startSearch.addEventListener("click", function (event) {
+    // historyOfTheCity.push(searchCity.value);
+    // localStorage.setItem("searchedCities", JSON.stringify(historyOfTheCity));
+    start(userInput)
     
-    event.preventDefault();
+}
 
-    startFunction();
-    historySaved(historyOfTheCity);
+function start (city) {
+    
+
+    // startFunction();
+    // historySaved(historyOfTheCity);
 
     
     //fetchData();
-    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${searchCity.value}&appid=8d6fedb89a89e930cd42aacc3d71bd01`)
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&units=imperial&appid=8d6fedb89a89e930cd42aacc3d71bd01`)
         .then(response => response.json())
         .then(geoData => {
-            console.log(geoData);
-            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${geoData[0].lat}&lon=${geoData[0].lon}&appid=8d6fedb89a89e930cd42aacc3d71bd01`)
-                .then(response => response.json())
-                .then(weatherData => {
-                    console.log(weatherData);
-                  
-                    //console.log(weatherList);
-                    //console.log(cityData);
-
-                })
-            console.log(geoData[0].lat);
-            console.log(geoData[0].lon);
+            runForecast(geoData);
+            runCurrentWeather(geoData)
+           
         })
 
 
-})
-
-
-function startFunction(){
-    searchForTheCity = searchCity.value;
-    console.log(searchCity.value);
-
-    historyOfTheCity.push(searchCity.value);
-    localStorage.setItem("searchedCities", JSON.stringify(historyOfTheCity));
-
-
 }
+
+function runCurrentWeather(geoData){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geoData[0].lat}&lon=${geoData[0].lon}&units=imperial&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(weatherData => {
+        console.log('THIS IS CURRENT WEATHER!!!',weatherData);
+
+
+        var card =  document.createElement('div')
+        var cardBody =  document.createElement('div')
+        var cardTitle = document.createElement('h3')
+        var cardText = document.createElement('p')
+        var cardText2 = document.createElement('p')
+        var cardText3 = document.createElement('p')
+      
+         card.textContent =  "card w-100";
+         sectionContainer.append(card);
+
+         cardBody.textContent = "card-body";
+         sectionContainer.append(cardBody);
+
+         cardTitle.textContent = "CITY";
+         sectionContainer.append(cardTitle);
+
+         cardText.textContent = "Temp";
+         sectionContainer.append(cardText);
+
+         cardText2.textContent = "humidity";
+         sectionContainer.append(cardText2);
+
+         cardText3.textContent = "wind";
+         sectionContainer.append(cardText3);
+
+    })
+}
+
+
+function runForecast(geoData){
+    console.log(geoData);
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${geoData[0].lat}&lon=${geoData[0].lon}&appid=8d6fedb89a89e930cd42aacc3d71bd01`)
+    .then(response => response.json())
+    .then(weatherData => {
+        console.log('THIS IS THE FORECAST!!!', weatherData);
+      
+        //console.log(weatherList);
+        //console.log(cityData);
+
+    })
+}
+
+
+
+
+
 
 function historySaved(){
     historyStored.innerHTML ="";
     for (var i = 0; i < historyOfTheCity.length; i++){
         var savedButton = document.getElementById("button");
         
-    
+        
         
 
     }
 }
 
-/*function fetchData(){
-    var apiKey = "8d6fedb89a89e930cd42aacc3d71bd01";
-    //fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${searchCity.value}&appid=${apiKey}`)
-    .then(response => response.json())
-    .then(geoData => {
-        //
-https://api.openweathermap.org/data/2.5/forecast?lat=42.3554334&lon=-71.060511&appid=8d6fedb89a89e930cd42aacc3d71bd01
-//https://api.openweathermap.org/geo/1.0/direct?q=boston&appid=8d6fedb89a89e930cd42aacc3d71bd01
-    //return fetch(`https://api.openweathermap.org/2.5/forecast?lat=${geoData[0].lat}&lon=${geoData[0].lon}&appid=${apiKey}&exclude=hourly,minutely&units=imperial`)
-    })
-    .then(response => response.json())
-    .then(cityData => {
 
-       
-
-        //pullCurrentData(cityData, searchForTheCity);
-        //futureData(cityData);
-  console.log(cityData);
-    })
-
-}*/
+startSearch.addEventListener("click", startFunction)
