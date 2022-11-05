9f678f174ea4cc343d8a811c411d334609d29962
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-11-05 16:54:52 -0500

    defined variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,14 @@ var currentDay = moment().format("h:mm:ss a");
 var searchCity = document.getElementById("city");
 var startSearch = document.getElementById("search");
 var historyStored = document.getElementById("history");
-var sectionContainer = document.getElementById("container")
+
+var cityInput = document.querySelector('#city-input');
+var city = document.querySelector('#city-name');
+
+var temp = document.querySelector('#temperature');
+var humid = document.querySelector('#humidity');
+var wind = document.querySelector('#wind');
+
 
 
 var searchForTheCity = "";
@@ -32,74 +39,74 @@ var searchForTheCity = "";
         
     }
 }*/
-function startFunction(){
-   var userInput = searchCity.value;
+function startFunction() {
+    var userInput = searchCity.value;
     console.log(userInput);
 
     // historyOfTheCity.push(searchCity.value);
     // localStorage.setItem("searchedCities", JSON.stringify(historyOfTheCity));
     start(userInput)
-    
+
 }
 
-function start (city) {
-    
+function start(city) {
+
 
     // startFunction();
     // historySaved(historyOfTheCity);
 
-    
+
     //fetchData();
     fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&units=imperial&appid=8d6fedb89a89e930cd42aacc3d71bd01`)
         .then(response => response.json())
         .then(geoData => {
             runForecast(geoData);
             runCurrentWeather(geoData)
-           
+
         })
 
 
 }
 
-function runCurrentWeather(geoData){
+function runCurrentWeather(geoData) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geoData[0].lat}&lon=${geoData[0].lon}&units=imperial&appid=${apiKey}`)
-    .then(response => response.json())
-    .then(weatherData => {
-        console.log('THIS IS CURRENT WEATHER!!!',weatherData);
+        .then(response => response.json())
+        .then(weatherData => {
+            console.log('THIS IS CURRENT WEATHER!!!', weatherData);
 
-  //city-Input
-  //city-name
-  //search
-  //temperature
-  //humidity
-  //wind
+            //city-Input
+            //city-name
 
-  
+            //temperature
+            //humidity
+            //wind
 
-
+           
 
 
-        
-        
-      
-         
 
-         
-    })
+
+
+
+
+
+
+
+        })
 }
 
 
-function runForecast(geoData){
+function runForecast(geoData) {
     console.log(geoData);
     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${geoData[0].lat}&lon=${geoData[0].lon}&appid=8d6fedb89a89e930cd42aacc3d71bd01`)
-    .then(response => response.json())
-    .then(weatherData => {
-        console.log('THIS IS THE FORECAST!!!', weatherData);
-      
-        //console.log(weatherList);
-        //console.log(cityData);
+        .then(response => response.json())
+        .then(weatherData => {
+            console.log('THIS IS THE FORECAST!!!', weatherData);
 
-    })
+            //console.log(weatherList);
+            //console.log(cityData);
+
+        })
 }
 
 
@@ -107,13 +114,13 @@ function runForecast(geoData){
 
 
 
-function historySaved(){
-    historyStored.innerHTML ="";
-    for (var i = 0; i < historyOfTheCity.length; i++){
+function historySaved() {
+    historyStored.innerHTML = "";
+    for (var i = 0; i < historyOfTheCity.length; i++) {
         var savedButton = document.getElementById("button");
-        
-        
-        
+
+
+
 
     }
 }
